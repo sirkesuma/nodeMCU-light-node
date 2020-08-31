3c64a7b3ed4c1175fa6cc7c0c5120bf702f3f537
Edo Kesuma <sirkesuma@live.com>
2020-08-31 21:25:41 +0700

    css

--- script.js
+++ script.js
@@ -1,48 +1,45 @@
-
 // api url 
-const api_url =  
-      "https://employeedetails.free.beeceptor.com/my/api/path"; 
-  
+const api_url = 
+	"http://192.168.43.234/"; 
+
 // Defining async function 
 async function getapi(url) { 
-    
-    // Storing response 
-    const response = await fetch(url); 
-    
-    // Storing data in form of JSON 
-    var data = await response.json(); 
-    console.log(data); 
-    if (response) { 
-        hideloader(); 
-    } 
-    show(data); 
+	
+	// Storing response 
+	const response = await fetch(url); 
+	
+	// Storing data in form of JSON 
+	var data = await response.json(); 
+	console.log(data); 
+	if (response) { 
+		hideloader(); 
+	} 
+	show(data); 
 } 
 // Calling that async function 
-getapi(api_url); 
-  
+var myVar = setInterval(getData, 1000);
+
+function getData(){
+    getapi(api_url);
+}
+
 // Function to hide the loader 
 function hideloader() { 
-    document.getElementById('loading').style.display = 'none'; 
+	document.getElementById('loading').style.display = 'none'; 
 } 
 // Function to define innerHTML for HTML table 
 function show(data) { 
-    let tab =  
-        `<tr> 
-          <th>Name</th> 
-          <th>Office</th> 
-          <th>Position</th> 
-          <th>Salary</th> 
-         </tr>`; 
-    
-    // Loop to access all rows  
-    for (let r of data.list) { 
-        tab += `<tr>  
-    <td>${r.name} </td> 
-    <td>${r.office}</td> 
-    <td>${r.position}</td>  
-    <td>${r.salary}</td>           
-</tr>`; 
-    } 
-    // Setting innerHTML as tab variable 
-    document.getElementById("employees").innerHTML = tab; 
+	let tab = 
+		`<tr> 
+		<th>Name</th> 
+		<th>Voltage</th> 
+		<th>Current</th> 
+		</tr>`; 
+	
+	// Loop to access all rows 
+	for (let r of data.Load) { 
+		tab += `<tr><td>${r.name} </td> <td>${r.voltage} ${r.voltage_unit}</td> <td>${r.current} ${r.current_unit}</td>	</tr>`; 
+	} 
+	// Setting innerHTML as tab variable 
+	document.getElementById("power_data").innerHTML = tab; 
 } 
